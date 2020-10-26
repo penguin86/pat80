879be841b3154372da051c15706a5fc41096ffc4
Daniele Verducci su MatissePenguin <daniele.verducci@gmail.com>
2020-10-26 22:05:03 +0100

    WIP BIOS

--- assembly/pat80_bios_0.1.asm
+++ assembly/pat80_bios_0.1.asm
@@ -1,34 +1,62 @@
+; Pat80 BIOS v0.01
+; @author: Daniele Verducci
+; 
+; ROM is at 0x00
+; RAM is at 0x80
+; LCD is at I/O 0x00 and 0x01
+
+jp sysinit     ; Startup vector: DO NOT MOVE! Must be the first instruction
+
+
 ; SYSTEM CONFIGURATION
 LCD_INSTR_REG: EQU %00000000
 LCD_DATA_REG: EQU %00000001
 
+
+; CONSTANTS
+SYSINIT_GREETING:
+    DB "Pat80 BIOS v0.1     ",0  ; null terminated string
+
+
+
+
+
+
+
+
+
+
 ; System initialization
-call lcd_init
+sysinit:
+    call lcd_init
 
-; write characters to display
-ld bc, hello_world
-call lcd_write      ; write string to screen
+    ; write characters to display
+    ld bc, SYSINIT_GREETING
+    call lcd_write      ; write string to screen
+    call lcd_cls        ; clear screen
 
-halt
+    halt
 
 
 lcd_init:
     ;reset procedure
-    ld a,%00111000
+    ld a,0x38
     out (LCD_INSTR_REG),a
-    ld a,%00001000
+    ld a,0x08
     out (LCD_INSTR_REG),a
-    ld a,%00000001
+    ld a,0x01
     out (LCD_INSTR_REG),a
 
     ;init procedure
-    ld a,%00111000
+    ld a,0x38
     out (LCD_INSTR_REG),a
-    ld a,%00001110
+    ld a,0x0F
     out (LCD_INSTR_REG),a
 
     ret
 
+; Writes text starting from current cursor position
+; @param BC Pointer to a null-terminated string first character
 lcd_write:
     ld a, (bc)  ; bc is the pointer to passed string's first char
     cp 0        ; compare A content with 0 (subtract 0 from value and set zero flag Z if result is 0)
@@ -37,5 +65,17 @@ lcd_write:
     inc bc ; increment bc to move to next char
     jp lcd_write
 
-hello_world:
-    DB "Lorem ipsum",0  ; null terminated string
+; Set cursor position
+; @param B X-axis position (0 to 19)
+; @param C Y-axis position (0 to 3)
+lcd_locate:
+    ; TODO
+    ret
+
+; Clears the screen
+lcd_cls:
+    ld a,0x01
+    out (LCD_INSTR_REG),a   ; clear display
+    ld a,0x02
+    out (LCD_INSTR_REG),a   ; cursor to home (top left)
+    ret
